8a4c6df61d7db7ff93113660741d37057b305adb
Annguyen011 <nguyenvanan01102003@gmail.com>
2024-05-07 09:33:41 +0700

    add

---
+++ Assets/_Data/Scripts/Models/ChessBoard.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+
+public class ChessBoard : MonoBehaviour
+{
+    #region [Elements]
+
+
+
+	#endregion
+
+
+	#region [Components]
+
+
+
+	#endregion
+
+	#region [Unity Methods]
+
+
+
+	#endregion
+
+	#region [Override]
+
+
+
+	#endregion
+
+}
