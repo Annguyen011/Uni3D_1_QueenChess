9fa2ed1b6297bf116856c39d49eb86deb7cc85a8
Annguyen011 <nguyenvanan01102003@gmail.com>
2024-05-07 08:49:04 +0700

    Init

---
+++ Assets/_Data/Scripts/Models/BlackCell.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+
+public class BlackCell : MonoBehaviour
+{
+    #region [Elements]
+
+
+
+	#endregion
+
+
+	#region [Components]
+
+
+
+	#endregion
+
+	#region [Unity Methods]
+
+
+
+	#endregion
+
+	#region [Override]
+
+
+
+	#endregion
+
+}
---
+++ Assets/_Data/Scripts/Models/Cell.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+
+public class Cell : MonoBehaviour
+{
+    #region [Elements]
+
+
+
+	#endregion
+
+
+	#region [Components]
+
+
+
+	#endregion
+
+	#region [Unity Methods]
+
+
+
+	#endregion
+
+	#region [Override]
+
+
+
+	#endregion
+
+}
